555eac314dcd1ea168c450fceeafb71f14943adb
zetvyy <zetvyy@gmail.com>
2023-06-08 09:26:48 +0700

    fix validation kuesioner

--- app/Views/pages/kuesioner1.php
+++ app/Views/pages/kuesioner1.php
@@ -4,7 +4,7 @@
 
 <h1 class="content">Isi Kuesioner</h1>
 <div class="container">
-    <form class="requires-validation shadow p-3 mb-5 bg-body-tertiary rounded" novalidate>
+    <form class="requires-validation shadow p-3 mb-5 bg-body-tertiary rounded myForm" novalidate>
         <div class="md-6">
             <label for="kampus" class="form-label">Perguruan Tinggi</label>
             <input type="text" class="form-control" id="kampus" />
@@ -31,21 +31,20 @@
             <!-- <div class="invalid-feedback">Please provide a valid city.</div> -->
         </div>
         <!-- Button trigger modal -->
-        <button onsubmit="validateForm()" type="submit" class="mt-3 btn btn-primary" id="submitButton" data-bs-toggle="modal" data-bs-target="#exampleModalCenter">
+        <button onsubmit="validateForm()" type="submit" class="mt-3 btn btn-primary" id="submitButton" data-bs-toggle="modal" data-bs-target="#myModal">
             Selanjutnya
         </button>
         <input class="mt-3 btn btn-danger" type="reset" id="reset" value="Batal" />
     </form>
 </div>
 <!-- Modal -->
-<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-body text-center">
-                <img src="/img/check.png" alt="checklogo" class="check_icon">
-                <p class="mt-3 font-weight-bold">Terima Kasih!</p>
-                <p>Validasi data berhasil!</p>
-                <a href="/kuesioner"><button class="btn btn-primary btn_modal">Lanjut</button></a>
+                <img alt="imageIcon" id="image_icon">
+                <p class=" mt-3" id="modal_message"></p>
+                <a href="/kuesioner" id="href_Atr"><button class="btn btn-primary" id="btn_modal">Lanjutkan</button></a>
             </div>
 
         </div>
@@ -54,17 +53,5 @@
 
 <script src="/js/form-action.js"></script>
 <script src="/js/validation.js"></script>
-<script>
-    // Validation kuseioner
-    const nim = document.getElementById("nim").nodeValue;
-    const tanggalLahir = document.getElementById("tanggalLahir").nodeValue;
-    const btnSubmit = document.querySelector("#submitButton");
 
-    const validateForm = (e) => {
-        if (nim !== "" && tanggalLahir !== "") {
-            e.preventDefault()
-            btnSubmit.classList.add()
-        }
-    }
-</script>
 <?= $this->endSection(); ?>
--- public/js/validation.js
+++ public/js/validation.js
@@ -25,3 +25,38 @@
     );
   });
 })();
+
+// Validation kuseioner
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector(".myForm");
+  const modalMessage = document.getElementById("modal_message");
+  const modalButton = document.getElementById("btn_modal");
+  const hrefButtonAtr = document.getElementById("href_Atr");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const nim = document.getElementById("nim").value;
+    const tanggalLahir = document.getElementById("tanggalLahir").value;
+    const imageIcon = document.getElementById("image_icon");
+
+    if (nim !== "" && tanggalLahir !== "") {
+      modalMessage.textContent = "Validasi Data Berhasil!";
+      imageIcon.src = "/img/check.png";
+
+      modalButton.disabled = false;
+      hrefButtonAtr.href = "/kuesioner";
+    } else {
+      modalMessage.textContent = "Validasi Data Gagal, coba lagi!";
+      imageIcon.src = "/img/cross.png";
+
+      modalButton.disabled = true;
+      hrefButtonAtr.href = "";
+
+      form.reset();
+    }
+
+    const modal = new bootstrap.Modal(document.getElementById("myModal"));
+    modal.show();
+  });
+});
